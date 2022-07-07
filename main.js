722475539186e253b71ae7e5642da5f782cb17ac
davide <dacapoccia@gmail.com>
2022-07-07 10:48:55 +0200

    FixedBuzz!

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 const containerDom = document.getElementById('container');
 
 for (let x=1; x<=100; x++){
-    const div = document.createElement('<div></div>');
-    let number = x;
-    
-    div.classList.add('box');
 
-    containerDom.appendChild('<div></div>');
+    
 
     if (x%3 == 0 && x%5 == 0){
-        div.classList.add('box1');
-        div.innerHTML = 'FizzBuzz';
-    } else if (x%3 == 0){
-        div.classList.add('box2');
-        div.innerHTML = 'fizz';
-    } else if (x%5 == 0){
-        div.classList.add('box3');
-        div.innerHTML = 'buzz';
-    } else {
-        div.innerHTML = 'number';
+        containerDom.innerHTML += "<div class='box box1'>FizzBuzz</div>"
+        console.log('FizzBuzz');
+    } 
+    else if (x%3 == 0){     
+        console.log('Fizz');
+        containerDom.innerHTML += "<div class='box box2'>Fizz</div>"
+
+
+    } 
+    else if (x%5 == 0){     
+        console.log('Buzz');
+        containerDom.innerHTML += "<div class='box box3'>Buzz</div>"
+
+
+    } 
+    else {
+        console.log(x);
+        containerDom.innerHTML += "<div class='box'>" + x + "</div>"
     }
 }
